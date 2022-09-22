8ade08c83ad0d66bf498a8c65b68808d99520ad7
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-22 16:57:18 +0800

    Add styling to card images set to circle

--- hello/src/components/Employee.js
+++ hello/src/components/Employee.js
@@ -1,8 +1,8 @@
 function Employee(props) {
   // props are in the parent component
   return (
-    <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-      <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face"/>
+    <div class="bg-red-300 m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img class="bg-red-200 object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt={props.name}/>
       <div class="text-center space-y-2 sm:text-left">
         <div class="space-y-0.5">
           <p class="text-lg text-black font-semibold">
