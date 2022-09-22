a7d8d83ab4f6f787e5a2a65f8122d1730d346b6f
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-22 13:36:37 +0800

    Set upuseState hook for role input

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [ role, setRole ] = useState();
   const showEmployee = true;
   return (
     <div className="App">
       {showEmployee ? (
         <>
+          <input type="text" onChange={
+            (e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }
+
+          } />
           <Employee name='Julia' role='intern'/> 
-          <Employee  name='Jane' /> 
+          <Employee  name='Jane' role={role}/> 
           <Employee  name='Robert' /> 
           <Employee /> 
           <Employee /> 
