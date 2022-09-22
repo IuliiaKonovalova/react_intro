f3a1db02d48ae31ed2cbce97bea9f4d02febc6b1
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-22 16:58:19 +0800

    Add show image functionality with props

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import image1 from './images/3d-fluency-businessman-1.png';
+import image2 from './images/3d-fluency-businesswoman-1.png';
+import image3 from './images/3d-fluency-carrot.png';
+import image4 from './images/3d-fluency-cherry.png';
+import image5 from './images/3d-fluency-crab.png';
 
 function App() {
   const [ role, setRole ] = useState();
@@ -16,11 +21,13 @@ function App() {
             }
 
           } />
-          <Employee name='Julia' role='intern'/> 
-          <Employee  name='Jane' role={role}/> 
-          <Employee  name='Robert' /> 
-          <Employee /> 
-          <Employee /> 
+          <div className="flex flex-wrap justify-center">
+            <Employee name='Julia' role='intern' img={image1}/> 
+            <Employee  name='Jane' role={role} img={image2}/> 
+            <Employee  name='Robert'  img={image3}/> 
+            <Employee name='Julia' role='intern' img={image4}/> 
+            <Employee  name='Jane' role={role}  img={image5}/> 
+            </div> 
         </>
       ): (
         <p>You cannot see employees</p>
