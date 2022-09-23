b1c6ef98a2736eeddd0213c4a66cbd5cd4f67b22
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-23 13:53:52 +0800

    Add edit employee data form

--- hello/src/components/EditEmployee.js
+++ hello/src/components/EditEmployee.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
+function EditEmployee(props) {
+
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -10,7 +12,7 @@ function EditEmployee() {
 
   return (
     <>
-        <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+        <button onClick={handleShow} className="px-4 py-1 text-sm text-purple-600 font-semibold rounded border ring-purple-600 border-purple-600 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
           Update
         </button>
 
@@ -21,17 +23,51 @@ function EditEmployee() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
+
+          <div className="w-full max-w-xs">
+            <form className="w-full max-w-small" id="editmodal">
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
+                    Full Name
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="name"
+                    type="text"
+                    value={props.name}
+                    // onChange={}
+                    />
+                </div>
+              </div>
+              <div className="md:flex md:items-center mb-6">
+                <div className="md:w-1/3">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" for="role">
+                    Role
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="role"
+                    type="text"
+                    value={props.role}
+                    // onChange={}
+                    />
+                </div>
+              </div>
+            </form>
+          </div>
+
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
+          <button className="bg-slate-400 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded" onClick={handleClose}>Cancel</button>
+          <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" form="editmodal">Update</button>
         </Modal.Footer>
       </Modal>
     </>
