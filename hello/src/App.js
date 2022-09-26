379342bd9a7069ceb1b14e94f15a8176235438d6
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-26 16:17:08 +0800

    Fix App return statement

--- hello/src/App.js
+++ hello/src/App.js
@@ -5,18 +5,20 @@ import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
 import Employees from './pages/Employees';
+import Customers from './pages/Customers';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <Header>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Header>
         <Routes>
           <Route path='/employees' element={<Employees />} />
+          <Route path='/customers' element={<Customers />} />
         </Routes>
-      </BrowserRouter>
-    </Header>
+      </Header>
+    </BrowserRouter>
   );
 }
 
