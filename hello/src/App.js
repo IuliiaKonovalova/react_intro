1c5021911a6b740009b428daa391f36766b06cf2
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-26 15:07:16 +0800

    Add react-route-dom

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
-import image1 from './images/3d-fluency-businessman-1.png';
-import image2 from './images/3d-fluency-businesswoman-1.png';
-import image3 from './images/3d-fluency-carrot.png';
-import image4 from './images/3d-fluency-cherry.png';
-import image5 from './images/3d-fluency-crab.png';
-import { v4 as uuidv4 } from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
 import Employees from './pages/Employees';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <Header>
-    <Employees />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/employees' element={<Employees />} />
+        </Routes>
+      </BrowserRouter>
     </Header>
   );
 }
