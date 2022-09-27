58df75a8d3c5ad860977d0921239ef87c2410af8
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-27 12:27:25 +0800

    Add navigate to 404 functionality on 404 response

--- hello/src/components/Definition.js
+++ hello/src/components/Definition.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Definition() {
 
   const [word, setWord] = useState("");
   let { search } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
-      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.status);
+        if (response.status === 404) {
+          navigate("/404");
+        }
+        response.json()
+      }
+      )
       .then((data) => {
         setWord(data[0].meanings);
       }
@@ -18,13 +26,17 @@ export default function Definition() {
 
   return (
     <>
-      <h1>Definition:</h1>
-      {word ? word.map((meaning) => (
-        (
-          <p key={uuidv4()}>
-            {meaning.partOfSpeech + ": " + meaning.definitions[0].definition}
-          </p>
-      )))
+      {word
+        ? 
+        <>
+        {word.map((meaning) => {
+          return (
+            <p key={uuidv4()}>
+              {meaning.partOfSpeech + ": " + meaning.definitions[0].definition}
+            </p>
+          )
+          })}
+          </>
       : null}
     </>
   )
