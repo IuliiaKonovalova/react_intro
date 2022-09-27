300541d9db89250ab6e035712efb03dbabc4e546
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-27 12:26:37 +0800

    Add route for page not found to App.js

--- hello/src/App.js
+++ hello/src/App.js
@@ -8,6 +8,7 @@ import Employees from './pages/Employees';
 import Customers from './pages/Customers';
 import Dictionary from './components/Dictionary';
 import Definition from './components/Definition';
+import NotFound from './components/404';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
@@ -18,9 +19,9 @@ function App() {
         <Routes>
           <Route path='/employees' element={<Employees />} />
           <Route path="/dictionary" element={<Dictionary />} />
-          <Route path="/definition" element={<Definition />} />
           <Route path="/definition/:search" element={<Definition />} />
           <Route path='/customers' element={<Customers />} />
+          <Route path='/404' element={<NotFound />} />
         </Routes>
       </Header>
     </BrowserRouter>
