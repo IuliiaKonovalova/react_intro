18887cce255bce8d4b0c00df68067d95f1cb8f17
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-27 12:26:07 +0800

    Add page not found component

---
+++ hello/src/components/404.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="App bg-slate-200 min-h-screen content-center max-w-120rem p-8 flex pt-8 flex-wrap justify-center">
+      <h1>404 - The page you are looking for is Not Found</h1>
+    </div>
+  )
+}
