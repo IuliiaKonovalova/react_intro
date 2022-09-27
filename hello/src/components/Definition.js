40cc61cd2cf45bc856b6b730726b9261e6e38d6e
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-27 18:13:04 +0800

    Add response status logic to definition component

--- hello/src/components/Definition.js
+++ hello/src/components/Definition.js
@@ -9,16 +9,24 @@ export default function Definition() {
 
   const [word, setWord] = useState("");
   const [notFound, setNotFound] = useState(false);
+  const [error, setError] = useState(false);
   let { search } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`)
       .then((response) => {
-        console.log(response.status);
-        console.log(response);
         if (response.status === 404) {
           setNotFound(true);
+        } else if (response.status === 401) {
+          navigate("/login");
+        } else if (response.status === 500) {
+          // setServerError(true);
+        }
+        if (!response.ok) {
+          setError(true);
+
+          throw new Error("Something went wrong");
         }
         return response.json();
         // response.json()
@@ -26,8 +34,11 @@ export default function Definition() {
       }
       )
       .then((data) => {
-        console.log(data);
         setWord(data[0].meanings);
+      }
+      )
+      .catch((error) => {
+        console.log(error.message);
       }
       );
   }, []);
@@ -48,7 +59,7 @@ export default function Definition() {
         <div className="App bg-slate-200 min-h-screen max-w-120rem flex pt-8 flex-wrap flex-column text-center">
           <Link to ='/dictionary'>Search another word</Link>
           <div className="flex flex-wrap flex-column justify-center mt-8">
-        {word.map((meaning) => {
+        {word.map((meaning) => { 
           return (
             <div className="flex flex-col">
             <p key={uuidv4()}>
