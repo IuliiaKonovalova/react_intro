8387cf3c9c3b9e67009525fedf94c4b20b913bbc
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-27 18:27:21 +0800

    Change dictionary return statement to form

--- hello/src/components/Dictionary.js
+++ hello/src/components/Dictionary.js
@@ -7,21 +7,26 @@ export default function Dictionary() {
 
   return (
     <div className="App bg-slate-200 min-h-screen max-w-120rem p-8">
-      <input
-        className="rounded-lg border-2 border-gray-600 mb-2"
-        type="text"
-        onChange={(e) => {
-          setWord(e.target.value)
-        }}
-      />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      <form
+        className="flex justify-center space-x-2 max-w-xl mx-auto bg-white rounded-lg shadow-lg p-8 flex-col mt-8"
         onClick={() => {
           navigate(`/definition/${word}`);
-        }}
-      >
-        Search
-      </button>
+        }}>
+        <h1 className="text-2xl text-gray-700 font-semibold text-center mb-4">Dictionary</h1>
+        <input
+          className="rounded-lg border-2 border-gray-600 mb-4 p-2"
+          placeholder="Search for a word"
+          type="text"
+          onChange={(e) => {
+            setWord(e.target.value)
+          }}
+        />
+        <button
+          className="bg-purple-500 hover:bg-purple-700 mx-auto text-white font-bold py-2 px-4 rounded"
+        >
+          Search more...
+        </button>
+      </form>
     </div>
   )
 }
