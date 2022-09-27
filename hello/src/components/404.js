a35e3d79b7696274724005950d65700ed8aa33f3
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-27 12:31:22 +0800

    Add styling to page 404

--- hello/src/components/404.js
+++ hello/src/components/404.js
@@ -1,7 +1,12 @@
 export default function NotFound() {
   return (
     <div className="App bg-slate-200 min-h-screen content-center max-w-120rem p-8 flex pt-8 flex-wrap justify-center">
-      <h1>404 - The page you are looking for is Not Found</h1>
+      <div className="bg-white rounded-lg shadow-lg p-8 m-4 max-w-sm w-full">
+        <div className="text-center">
+          <h1 className="text-gray-700">404</h1>
+          <p className="text-gray-800">Page not found</p>
+        </div>
+      </div>
     </div>
   )
 }
