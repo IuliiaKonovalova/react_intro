bb6f99ac5de48f921214564db807136fb4889901
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-29 17:51:06 +0800

    Fix customer state and add basic styling for customers page

--- hello/src/pages/Customers.js
+++ hello/src/pages/Customers.js
@@ -8,14 +8,23 @@ export default function Customers() {
     fetch('http://localhost:8000/api/customers/')
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setCustomers(data);
+        console.log(data.data);
+        setCustomers(data.data);
       }
       );
   }, []);
+  
   return (
-    <div className='bg-slate-200 flex pt-8 flex-wrap justify-center min-h-screen'>
-      <h1>Customers</h1>
+    <div className='bg-slate-200 flex pt-8 flex-wrap flex-col min-h-screen'>
+      <h1 className='text-4xl text-center'>Customers</h1>
+
+    
+      <div className='flex flex-wrap justify-center flex-col mt-8'>
+        {customers ? customers.map((customer) => {
+          console.log("Customer", customer);
+          return <p className="text-center p-2 mb-2 bg-white">{customer.name}</p>
+        }) : null}
+      </div>
     </div>
   );
 }
