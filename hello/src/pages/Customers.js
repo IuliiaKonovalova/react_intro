6b14290f57e3e413a28c91a69d56a43401178bd5
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-29 16:33:25 +0800

    Add fetching customers data functionality

--- hello/src/pages/Customers.js
+++ hello/src/pages/Customers.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
+
 export default function Customers() {
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    console.log("Fetching");
+    fetch('http://localhost:8000/api/customers/')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCustomers(data);
+      }
+      );
+  }, []);
   return (
     <div className='bg-slate-200 flex pt-8 flex-wrap justify-center min-h-screen'>
       <h1>Customers</h1>
