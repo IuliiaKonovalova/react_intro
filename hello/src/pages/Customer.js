0beb1976b4aa6cca4445e258ff8213e9b6265953
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-09-30 15:45:23 +0800

    Add stylings to customer detail page

--- hello/src/pages/Customer.js
+++ hello/src/pages/Customer.js
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 
 export default function Customer() {
   const [customer, setCustomer] = useState([]);
-  const [NotFound, setNotFound] = useState([]);
+  const [notFound, setNotFound] = useState(false);
+
   const navigate = useNavigate();
   const { id } = useParams();
   useEffect(() => {
@@ -15,9 +16,9 @@ export default function Customer() {
     fetch(url)
       .then((res) => {
         if (res.status === 404) {
-          navigate('/404');
-        } else if (res.status === 405) {
+          // navigate('/404');
           setNotFound(true);
+        } else if (res.status === 405) {
         } else if (!res.ok) {
           throw Error('could not fetch the data for that resource');
         }
@@ -29,6 +30,25 @@ export default function Customer() {
   }, []);
   return (
     <>
+      {notFound ? <h1>Customer with id {id } not found</h1> : null }
+      <div className="App bg-slate-200 min-h-screen  max-w-120rem p-8 flex pt-8 flex-wrap justify-center">
+        {customer?.id ? 
+          <div className="bg-white rounded-lg shadow-lg h-40 max-h-full p-8 m-4 max-w-sm w-full flex flex-col text-center justify-center">
+            <div
+              className="bg-cover bg-center w-40 rounded-full mx-auto ">
+                <span className="font-bold">ID:</span> {customer.id}
+            </div> 
+            <div
+              className="bg-cover bg-center w-40 rounded-full mx-auto ">
+                <span className="font-bold">Name:</span> {customer.name}
+            </div> 
+            <div
+              className="bg-cover bg-center w-40 rounded-full mx-auto ">
+                <span className="font-bold">Industry:</span> {customer.industry}
+            </div>
+          </div>
+        : null}
+      </div>
     </>
 
   )
