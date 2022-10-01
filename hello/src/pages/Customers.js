c975820c1ea49b9db6de06f008b0959c40d32873
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-01 21:02:30 +0800

    Add logic for frontend adding customer

--- hello/src/pages/Customers.js
+++ hello/src/pages/Customers.js
@@ -1,9 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import AddCustomer from "../components/AddCustomer";
 import { baseUrl } from "../shared";
 
 export default function Customers() {
   const [customers, setCustomers] = useState([]);
+    //const [customers, setCustomers] = useState();
+  const [show, setShow] = useState(false);
+
+  function toggleShow() {
+      setShow(!show);
+  }
   useEffect(() => {
     console.log("Fetching");
 
@@ -17,18 +24,59 @@ export default function Customers() {
       }
       );
   }, []);
+
+  function newCustomer(name, industry) {
+    const data = { name: name, industry: industry };
+    const url = baseUrl + 'api/customers/';
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Something went wrong');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            toggleShow();
+            console.log(data);
+            setCustomers([...customers, data.customer]);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+
+  }
   
   return (
     <div className='bg-slate-200 flex pt-8 flex-wrap flex-col min-h-screen'>
       <h1 className='text-4xl text-center'>Customers</h1>
 
     
-      <div className='flex flex-wrap justify-center flex-col mt-8'>
+      <div className='flex flex-wrap justify-center flex-col mt-8 items-center'>
         {customers ? customers.map((customer) => {
           console.log("Customer", customer);
-          return <Link to={"/customer/" + customer.id} className="text-center p-2 mb-2 bg-white">{customer.name}</Link>
+          return (
+            <div className="m-2" key={customer.id}>
+                              <Link to={'/customers/' + customer.id}>
+                                  <button 
+                                    className="flex pt-8 flex-wrap flex-col no-underline bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+                                      {customer.name}
+                                  </button>
+                              </Link>
+                          </div>
+          )
         }) : null}
       </div>
+      <AddCustomer
+                newCustomer={newCustomer}
+                show={show}
+                toggleShow={toggleShow}
+            />
     </div>
   );
 }
