f33d53db5f28fb50aaf4fb3e36f2880dea7058e3
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-02 20:37:09 +0800

    Add login logic to the login page

--- hello/src/pages/Login.js
+++ hello/src/pages/Login.js
@@ -1,23 +1,53 @@
 import { useParams, Link, useNavigate, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { baseUrl } from "../shared";
+import { useLocation } from "react-router-dom";
+import { LoginContext } from "../App";
+
 
 export default function Login() {
-  // const { user } = useAuth();
+
+  const [loggedIn, setLoggedIn] = useContext(LoginContext);
+
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+
+  const location = useLocation();
+
   const navigate = useNavigate();
 
-function login() {
-  const url = baseUrl + 'api/login';
+  function login(e) {
+    e.preventDefault();
+    const url = baseUrl + 'api/token/';
 
-  
-}
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: username, password: password})
+      })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('could not login');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        localStorage.setItem('access', data.access);
+        localStorage.setItem('refresh', data.refresh);
+        setLoggedIn(true)
+        navigate(location?.state?.previousUrl ? location.state.previousUrl : '/customers');
+      })
+  }
 
 return (
   <div
     className='App bg-slate-200 min-h-full max-w-120rem flex mt-20 flex-wrap justify-center flex-col content-center'>
-<form
+      <form
             id='customer'
-            // onSubmit={}
+            onSubmit={login}
             className="bg-white rounded-lg shadow-lg  max-h-full p-10 m-4 max-w-sm w-full flex flex-col text-center justify-center">
             <h1 className="text-2xl font-bold text-gray-900">Login</h1>
 
@@ -27,10 +57,10 @@ return (
                 <input 
                   className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                   type='text'
-                  value={''}
+                  value={username}
                   id='username'
                   onChange={(e) => {
-
+                    setUsername(e.target.value);
                   }} />
             </div> 
             <div
@@ -39,10 +69,10 @@ return (
                 <input
                   id='password'
                   className="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                  type='text'
-                  value={''}
+                  type='password'
+                  value={password}
                   onChange={(e) => {
-
+                    setPassword(e.target.value);
                   }} />
             </div>
             {/* {changed ? */}
@@ -60,7 +90,7 @@ return (
                   // onClick=
                   //   {}
                   >
-                    Save
+                    Login
                 </button>
               </div>
             {/* : null} */}
