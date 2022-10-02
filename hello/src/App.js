9ba9ed4cc7ed2491d7d6a4d5c262e2ed358bc41d
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-02 20:36:09 +0800

    Add Login context provider to ap function

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
-import AddEmployee from './components/AddEmployee';
-import EditEmployee from './components/EditEmployee';
+import { createContext, useState } from 'react';
 import Header from './components/Header';
 import Employees from './pages/Employees';
 import Customers from './pages/Customers';
@@ -11,26 +8,35 @@ import Definition from './components/Definition';
 import NotFound from './components/404';
 import NoResults from './components/NoResults';
 import Customer from './pages/Customer';
+import Login from './pages/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
+export const LoginContext = createContext();
+
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Header>
-        <Routes>
-          {/* <Route path='/home' element={<Home />} /> */}
-          <Route path='/employees' element={<Employees />} />
-          <Route path="/dictionary" element={<Dictionary />} />
-          <Route path="/definition/:search" element={<Definition />} />
-          <Route path='/customers' element={<Customers />} />
-          <Route path='/customers/:id' element={<Customer />} />
-          <Route path='/404' element={<NotFound />} />
-          <Route path='*' element={<NotFound />} />
-          <Route path='/NoResults' element={<NoResults />} />
-        </Routes>
-      </Header>
-    </BrowserRouter>
+    <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
+      <BrowserRouter>
+        <Header>
+          <Routes>
+            {/* <Route path='/home' element={<Home />} /> */}
+            <Route path='/employees' element={<Employees />} />
+            <Route path="/dictionary" element={<Dictionary />} />
+            <Route path="/definition/:search" element={<Definition />} />
+            <Route path='/customers' element={<Customers />} />
+            <Route path='/customers/:id' element={<Customer />} />
+            <Route path='/404' element={<NotFound />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<NotFound />} />
+            <Route path='/NoResults' element={<NoResults />} />
+          </Routes>
+        </Header>
+      </BrowserRouter>
+    </LoginContext.Provider>
   );
 }
 
