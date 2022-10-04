718f940b2589f2a26ffdfcac9e3d57a32574bc44
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-04 14:22:10 +0800

    Change structure of customers.js with custom hook

--- hello/src/pages/Customers.js
+++ hello/src/pages/Customers.js
@@ -3,13 +3,14 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 import AddCustomer from "../components/AddCustomer";
 import { baseUrl } from "../shared";
 import { LoginContext } from "../App";
+import useFetch from "../hooks/UseFetch";
 
 
 export default function Customers() {
 
   const [loggedIn, setLoggedIn] = useContext(LoginContext);
 
-  const [customers, setCustomers] = useState([]);
+  // const [customers, setCustomers] = useState([]);
 
   const [show, setShow] = useState(false);
 
@@ -20,64 +21,50 @@ export default function Customers() {
   }
 
   const location = useLocation();
-
-
-
   const navigate = useNavigate();
+
+  const url = baseUrl + 'api/customers/';
+
+  const { request, appendData, data: { customers } = {}, errorStatus } = useFetch(url, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('access'),
+    },
+  });
+
   useEffect(() => {
     console.log("Fetching");
 
-    const url = baseUrl + `api/customers/`;
-
-    fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + localStorage.getItem('access')
-      }
-    })
-      .then((response) => {
-        if (response.status === 401) {
-          setLoggedIn(false);
-          navigate('/login', {
-            state: {
-              previousUrl: location.pathname,
-            }
-          });
-        }
-        return response.json()
-      })
-      .then((data) => {
-        console.log(data.customers);
-        setCustomers(data.customers);
-      }
-      );
+    request();
   }, []);
 
   function newCustomer(name, industry) {
-    const data = { name: name, industry: industry };
-    const url = baseUrl + 'api/customers/';
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + localStorage.getItem('access')
-        },
-        body: JSON.stringify(data),
-    })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('Something went wrong');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            toggleShow();
-            console.log(data);
-            setCustomers([...customers, data.customer]);
-        })
-        .catch((e) => {
-            console.log(e);
-        });
+    appendData({ name: name, industry: industry });
+    // const data = { name: name, industry: industry };
+    // const url = baseUrl + 'api/customers/';
+    // fetch(url, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         Authorization: 'Bearer ' + localStorage.getItem('access')
+    //     },
+    //     body: JSON.stringify(data),
+    // })
+    //     .then((response) => {
+    //         if (!response.ok) {
+    //             throw new Error('Something went wrong');
+    //         }
+    //         return response.json();
+    //     })
+    //     .then((data) => {
+    //         toggleShow();
+    //         console.log(data);
+    //         setCustomers([...customers, data.customer]);
+    //     })
+    //     .catch((e) => {
+    //         console.log(e);
+    //     });
 
   }
   
