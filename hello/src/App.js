1266264f11f0bcf556465e782ba317d3f326d5e8
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-10-04 14:23:08 +0800

    Add route for register page to App.js

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Employees from './pages/Employees';
 import Customers from './pages/Customers';
@@ -9,12 +9,41 @@ import NotFound from './components/404';
 import NoResults from './components/NoResults';
 import Customer from './pages/Customer';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { baseUrl } from './shared';
 
 
 export const LoginContext = createContext();
 
 function App() {
+
+  useEffect(() => {
+    function refreshToken() {
+      if (localStorage.refresh) {
+        const url = baseUrl + 'api/token/refresh/';
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            refresh: localStorage.refresh,
+          })
+        })
+          .then(res => res.json())
+          .then((data) => {
+            console.log(data);
+            localStorage.access = data.access;
+            localStorage.refresh = data.refresh;
+            setLoggedIn(true);
+          })
+        }
+    }
+    const minutes = 100 * 60;
+    refreshToken();
+    setInterval(refreshToken, minutes * 3);
+  }, []);
   // check localStorage for an access token --> may be expired
   const [loggedIn, setLoggedIn] = useState(localStorage.access ? true : false);
   // long term goal --> use Refresh token and if is works, stay logged in, otherwise, log out and send to login page
@@ -41,6 +70,7 @@ function App() {
             <Route path='/customers/:id' element={<Customer />} />
             <Route path='/404' element={<NotFound />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
             <Route path='*' element={<NotFound />} />
             <Route path='/NoResults' element={<NoResults />} />
           </Routes>
